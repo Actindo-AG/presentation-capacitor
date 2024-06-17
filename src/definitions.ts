9186ab0ba824ab9383d40a19570eb30ccafac2d1
Dan <danci1159@gmail.com>
2024-06-17 13:14:46 +0200

    rework java files

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,23 +1,29 @@
 import { PluginListenerHandle } from '@capacitor/core';
-
 export interface CapacitorPresentationPlugin {
-  /**
-   *
-   * @param options
-   * @param url works only web
-   * @param htmlStrings works only android
-   */
-  openLink(options: OpenLinkOptions): Promise<{ success?: any; error?: any; url?: any }>;
-  addListener(
-    eventName: 'onSuccessLoadUrl',
-    listenerFunc: (data: any) => void,
-  ): Promise<PluginListenerHandle> & PluginListenerHandle;
-  addListener(
-    eventName: 'onFailLoadUrl',
-    listenerFunc: (data: any) => void,
-  ): Promise<PluginListenerHandle> & PluginListenerHandle;
-
-  getDisplays(): Promise<{ displays: number }>;
+    /**
+     *
+     * @param options
+     * @param url works only web
+     * @param htmlStrings works only android
+     */
+    openLink(options: OpenLinkOptions): Promise<{
+        success?: any;
+        error?: any;
+        url?: any;
+    }>;
+    addListener(eventName: 'onSuccessLoadUrl', listenerFunc: (data: any) => void): Promise<PluginListenerHandle> & PluginListenerHandle;
+    addListener(eventName: 'onFailLoadUrl', listenerFunc: (data: any) => void): Promise<PluginListenerHandle> & PluginListenerHandle;
+    getDisplays(): Promise<{
+        displays: number;
+    }>;
 }
+export declare type OpenLinkOptions = {
+    url: string;
+    htmlStrings?: string;
+    baseUrl?: string;
+} | {
+    htmlStrings: string;
+    url?: string;
+    baseUrl?: string;
+};
 
-export type OpenLinkOptions = {url: string, htmlStrings?: string} | {htmlStrings: string, url?: string}
